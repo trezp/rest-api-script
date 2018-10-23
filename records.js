3161ca970505ffbc21f907cc4dbb8383d4080408
Treasure <treasure.rose@gmail.com>
2018-10-23 13:53:30 -0700

    second GET route

---
+++ records.js
@@ -0,0 +1,65 @@
+const fs = require('fs');
+const data = require('./data.json');
+
+function generateRandomId(){
+  return Math.floor(Math.random() * 10000);
+}
+
+function save(){
+  return new Promise((resolve, reject) => {
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+async function getAll(){
+  return data.records;
+}
+
+async function getOne(id){
+  return data.records.find(record => record.id == id);
+}
+
+async function getRandom(arr){
+  const randNum = Math.floor(Math.random() * data.records.length);
+  return arr[randNum];
+}
+
+async function create(body) {
+  const record = {
+    id: generateRandomId(),
+    quote: body.quote,
+    author: body.author,
+    year: body.year
+  }
+  data.records.push(record);
+  await save(); 
+  return record; 
+}
+
+async function edit(record, body){
+  record.quote = body.quote || record.quote;
+  record.author = body.author || record.author;
+  record.year = body.year || record.year;
+  await save();
+}
+
+async function deleteRecord(record){
+  data.records = data.records.filter(item => item.id != record.id);
+  console.log(data.records)
+  return save();
+}
+
+module.exports = {
+  getAll,
+  getOne, 
+  create, 
+  edit, 
+  deleteRecord,
+  getRandom
+}
