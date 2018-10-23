715ecd22b8ca4509cb5b3864315b1b000859b9a7
Treasure <treasure.rose@gmail.com>
2018-10-23 14:05:40 -0700

    End of stage 1

--- app.js
+++ app.js
@@ -2,9 +2,25 @@ const express = require('express');
 
 const app = express();
 
+// Send a GET request to view (READ) a list of quotes 
 app.get('/', (req, res) => {
   res.json(quotes);
 });
+
+// Send a GET request to view (READ) a quote
+app.get('/:index', (req, res) => {
+  res.json(quotes.quotes[req.params.index]);
+});
+
+// Send a GET request to view (READ) a random quote
+// Send a POST request to CREATE a new quote 
+// Send a PUT request to UPDATE a quote 
+// Send a DELETE request to DELETE a QUOTE
+
+
+
+
+
 
 app.listen(3000, () => console.log('Quote API listening on port 3000!'));
 
