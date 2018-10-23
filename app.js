7da19cf4931ef8abbac14ac22f53c742c550997a
Treasure <treasure.rose@gmail.com>
2018-10-23 13:52:59 -0700

    first GET route

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.json(quotes);
+});
+
 app.listen(3000, () => console.log('Quote API listening on port 3000!'));
 
 const quotes = {
