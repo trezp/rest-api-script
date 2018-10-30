b1b83fcbc1d6efdc324a023632c08d37bbdcaca0
Treasure <treasure.rose@gmail.com>
2018-10-30 09:59:24 -0700

    error handling

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ const app = express();
 
 app.use(express.json()); 
 
+app.use((req, res, next) => {
+  req.message = "I created some middleware";
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log(req.message);
+  next();
+});
+
+
 // Send a GET request to view (READ) a list of quotes 
 app.get('/', async (req, res) => {
   const quotes = await records.fakeServerDelay(records.getAll);
@@ -25,5 +36,21 @@ app.post('/', async (req,res) => {
 });
 // Send a PUT request to UPDATE a quote 
 // Send a DELETE request to DELETE a QUOTE
+
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      message: err.message
+    } 
+  });
+
+});
 
 app.listen(3000, () => console.log('Quote API listening on port 3000!'));
