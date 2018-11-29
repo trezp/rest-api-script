2d46f22eaa035a3e2aab32b96b89dce3993e544b
Treasure <treasure.rose@gmail.com>
2018-11-28 16:33:38 -0800

    Updated method names

--- records.js
+++ records.js
@@ -20,7 +20,7 @@ function save(data){
  * Gets all quotes
  * @param None
  */
-function getAll(){
+function getQuotes(){
   return new Promise((resolve, reject) => {
     fs.readFile('data.json', 'utf8', (err, data) => {
       if (err) {
@@ -37,16 +37,16 @@ function getAll(){
  * Gets a specific quote by ID
  * @param {number} id - Accepts the ID of the specified quote.
  */
-async function getOne(id){
-  const quotes = await getAll();
+async function getQuote(id){
+  const quotes = await getQuotes();
   return quotes.records.find(record => record.id == id);
 }
 /**
  * Gets a random quote 
  * @param None
  */
-async function getRandom(){
-  const quotes = await getAll();
+async function getRandomQuote(){
+  const quotes = await getQuotes();
   console.log(quotes)
   const randNum = Math.floor(Math.random() * quotes.records.length);
   return quotes.records[randNum];
@@ -56,8 +56,8 @@ async function getRandom(){
  * Creates a new quote record 
  * @param {Object} newRecord - Object containing info for new quote: the quote text, author and year 
  */
-async function create(newRecord) {
-  const quotes = await getAll(); 
+async function createQuote(newRecord) {
+  const quotes = await getQuotes(); 
 
   if (newRecord.quote && newRecord.author){
     newRecord.id = generateRandomId(); 
@@ -74,8 +74,8 @@ async function create(newRecord) {
  * @param {Object} record - The quote to be changed
  * @param {Object} body - An object containing the changes to quote: quote, author, year (all optional)
  */
-async function edit(record, body){
-  const quotes = await getAll();
+async function updateQuote(record, body){
+  const quotes = await getQuote();
   const quote = quotes.records.find(item => item.id == record.id);
   
 
@@ -90,17 +90,17 @@ async function edit(record, body){
  * Deletes a single record
  * @param {Object} record - Accepts record to be deleted. 
  */
-async function deleteRecord(record){
-  const quotes = await getAll();
+async function deleteQuote(record){
+  const quotes = await getQuotes();
   quotes.records = quotes.records.filter(item => item.id != record.id);
   await save(quotes);
 }
 
 module.exports = {
-  getAll,
-  getOne, 
-  create, 
-  edit, 
-  deleteRecord,
-  getRandom
+  getQuotes,
+  getQuote, 
+  createQuote, 
+  updateQuote, 
+  deleteQuote,
+  getRandomQuote
 }
--- routes.js
+++ routes.js
@@ -17,24 +17,24 @@ function asyncHandler(cb){
 
 // Send a GET request to view (READ) a list of quotes 
 router.get('/quotes', async (req, res) => {
-  const quotes =  await records.getAll();
+  const quotes =  await records.getQuotes();
   res.json(quotes);
 });
 
 // Send a GET request to view (READ) a quote
 router.get('/quotes/:id', async (req, res) => {
-  const quote = await records.getOne(req.params.id);
+  const quote = await records.getQuote(req.params.id);
   res.json(quote);
 });
 
 router.get('/quotes/quote/random', asyncHandler (async(req, res, next)=>{
-  const quotes = await records.getAll();
-  const quote = await records.getRandom(quotes);
+  const quotes = await records.getQuotes();
+  const quote = await records.getRandomQuote(quotes);
   res.json(quote);
 }));
 
 router.post('/quotes', asyncHandler( async (req, res) => {
-  const quote = await records.create({
+  const quote = await records.createQuote({
     "quote": req.body.quote, 
     "author": req.body.author,
     "year": req.body.year
@@ -45,16 +45,16 @@ router.post('/quotes', asyncHandler( async (req, res) => {
 // Send a PUT request to UPDATE a quote 
 // Edit quote
 router.put('/quotes/:id', asyncHandler( async(req,res, next)=>{
-    const quote = await records.getOne(req.params.id);
-    await records.edit(quote, req.body);
+    const quote = await records.getQuote(req.params.id);
+    await records.updateQuote(quote, req.body);
     res.status(204).end();
 })); 
 
 // Send a DELETE request to DELETE a QUOTE
 router.delete('/quotes/:id', async(req,res,)=>{
   try {
-    const quote = await records.getOne(req.params.id);
-    await records.deleteRecord(quote);
+    const quote = await records.getQuote(req.params.id);
+    await records.deleteQuote(quote);
     res.status(204).end();
   } catch(err){
     es.status(500).json({ message: err.message });
