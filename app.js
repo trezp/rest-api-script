759fdaa815a55bc65f7bbee862831908358923c7
Treasure <treasure.rose@gmail.com>
2018-12-16 12:47:27 -0800

    script edit complete

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ app.use(express.json());
 
 app.use('/api', routes);
 
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.json({
     error: {
--- routes.js
+++ routes.js
@@ -21,13 +21,13 @@ router.get('/quotes', async (req, res) => {
 });
 
 // Send a GET request to view (READ) a quote
-router.get('/quotes/:id', async (req, res) => {
+router.get('/quotes/:id', async (req, res, next) => {
   try {
     const quote = await records.getQuote(req.params.id);
     if (quote){
       res.json(quote);
     } else {
-      res.status(404).json({message: "Quote not found"});
+      res.status(404).json({error: {message: "Quote not found"}});
     }
   } catch(err){
     res.status(500).json({message: err.message});
@@ -79,29 +79,44 @@ router.post('/quotes', async (req, res) => {
 // })); 
 
 router.put('/quotes/:id', asyncHandler( async(req,res)=>{
-  throw new Error("Error!")
-  // const quote = await records.getQuote(req.params.id);
-  // if(quote){
-  //   quote.quote = req.body.quote;
-  //   quote.author = req.body.author; 
-  //   quote.year = req.body.year; 
+  const quote = await records.getQuote(req.params.id);
+  if(quote){
+    quote.quote = req.body.quote;
+    quote.author = req.body.author; 
+    quote.year = req.body.year; 
 
-  //   await records.updateQuote(quote);
-  //   res.status(204).end(); 
-  // } else {
-  //   res.status(404).json({message: "Quote not found"});
-  // }
+    await records.updateQuote(quote);
+    res.status(204).end(); 
+  } else {
+    res.status(404).json({error: {message: "Quote not found"}});
+  }
 })); 
 
 // Send a DELETE request to DELETE a QUOTE
-router.delete('/quotes/:id', async(req,res)=>{
-  try {
-    const quote = await records.getQuote(req.params.id);
+// router.delete('/quotes/:id', async(req,res, next)=>{
+//   const quote = await records.getQuote(req.params.id);
+//   try {
+//     throw new Error("Something bad happened");
+//     if (quote){
+//       await records.deleteQuote(quote);
+//       res.status(204).end();
+//     } else {
+//       res.status(404).json({message: "Quote not found"});
+//     }
+//   } catch(err){
+//     next(err);
+//   }
+// });
+
+router.delete('/quotes/:id', asyncHandler(async(req,res)=>{
+  //throw new Error("Something bad happened");
+  const quote = await records.getQuote(req.params.id);
+  if (quote){
     await records.deleteQuote(quote);
     res.status(204).end();
-  } catch(err){
-    res.status(500).json({ message: err.message });
+  } else {
+    res.status(404).json({error: {message: "Quote not found"}});
   }
-});
+}));
 
 module.exports = router;
